6b00d45c370928798a0a180488df2245ab3057dc
Ramesh1243 <57915060+Ramesh1243@users.noreply.github.com>
2020-01-22 11:21:50 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -26,6 +26,6 @@ public class App
          */
         System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
         System.out.println("Hello All");
-        System.out.println("Hello All 01");
+        System.out.println("Hello All 05");
     }
 }
